b542a70fbd0d939e59d69115caae7d0ee87600c3
Vladimir Migashko <migashko@gmail.com>
2014-03-17 12:41:28 +0400

    upd

--- module/api/types_json.hpp
+++ module/api/types_json.hpp
@@ -7,8 +7,7 @@
 namespace wamba{ namespace demo{
 
 typedef wfc::json::array< 
-  std::vector< wfc::json::value<int> >,
-  fas::int_<10>
+  std::vector< wfc::json::value<int> >, 10
 > data_type_json;
 
 typedef wfc::json::pointer<data_ptr, data_type_json> data_ptr_json;
