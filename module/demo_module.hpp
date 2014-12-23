dca2594059ecb4dcd5f7a7190e11dfaadbc19f79
Vladimir Migashko <migashko@gmail.com>
2014-12-23 21:15:00 +0300

    +module

--- module/demo_module.hpp
+++ module/demo_module.hpp
@@ -25,22 +25,3 @@ public:
   
 }}
 
-/*
-#pragma once
-
-
-#include <wfc/module/multi_instance.hpp>
-#include "demo_instance.hpp"
-#include "demo_config_json.hpp"
-#include "demo_build_info.h"
-
-namespace wamba{ namespace demo{
-
-class demo_module
-  : public wfc::multi_instance<demo_build_info, demo_config_json, demo_instance>
-{
-  
-};
-
-}}
-*/
