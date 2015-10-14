dcb2b2cd3e256f6e47d6f7f55ae6ea78d15258c6
Vladimir Migashko <migashko@gmail.com>
2015-10-14 20:30:28 +0300

    upd

--- demo/pingpong/pingpong.cpp
+++ demo/pingpong/pingpong.cpp
@@ -11,6 +11,9 @@
 #include <atomic>
 #include <memory>
 #include <chrono>
+#include <iomanip>
+#include <boost/concept_check.hpp>
+
 
 #define PINGPONG_LOG_MESSAGE(message) WFC_LOG_MESSAGE("pingpong", message)
 
@@ -40,14 +43,14 @@ namespace {
     {
       using namespace std::placeholders;
       auto pong_req = std::make_unique<request::pong>();
-      pong_req->ping_count = req->ping_count + 1;
+      pong_req->ping_count = req->ping_count;
       pong_req->pong_count = req->pong_count;
       pong_reqester( std::move(pong_req), std::bind( pong_result_ping, cb, _1 ) );
     }
     else
     {
       auto res = std::make_unique<response::ping>();
-      res->ping_count = req->ping_count + 1;
+      res->ping_count = req->ping_count;
       res->pong_count = req->pong_count;
       cb( std::move(res));
     }
@@ -56,19 +59,22 @@ namespace {
   response::ping::handler make_response_wrapper(size_t size, response::ping::handler cb, ipingpong::pong_handler pong_reqester)
   {
     typedef std::shared_ptr< std::atomic_size_t > size_ptr;
-    std::pair<size_ptr, size_ptr> p;
-    p.first = std::make_shared< std::atomic_size_t >( size );
-    p.second = std::make_shared< std::atomic_size_t >( 0 );
+    std::tuple<size_ptr, size_ptr, size_ptr> p;
+    std::get<0>(p) = std::make_shared< std::atomic_size_t >( size );
+    std::get<1>(p) =  std::make_shared< std::atomic_size_t >( 0 );
+    std::get<2>(p) =  std::make_shared< std::atomic_size_t >( 0 );
     return [cb, p, pong_reqester](response::ping::ptr res)
     {
-      auto& callback_counter = *(p.first);
-      auto& ping_counter = *(p.second);
+      auto& callback_counter = *std::get<0>(p);
+      auto& ping_counter = *std::get<1>(p);
+      auto& pong_counter = *std::get<2>(p);
       --callback_counter;
       if ( callback_counter == 0)
       {
         if ( res != nullptr )
         {
           res->ping_count += ping_counter;
+          res->pong_count += pong_counter;
         }
         if ( pong_reqester!=nullptr )
         {
@@ -82,6 +88,7 @@ namespace {
       else if ( res != nullptr )
       {
         ping_counter += res->ping_count;
+        pong_counter += res->pong_count;
       }
     };
   }
@@ -136,7 +143,7 @@ void pingpong::reping_(request::ping::ptr req, response::ping::handler cb, io_id
 {
   using namespace std::placeholders;
   request::ping req_ping;
-  req_ping.ping_count = req->ping_count + 1;
+  req_ping.ping_count = req->ping_count;
   req_ping.pong_count = req->pong_count;
 
   auto callback = make_response_wrapper(_targets.size(), std::move(cb), std::move(pong_reqester) );
@@ -160,6 +167,7 @@ void pingpong::ping(request::ping::ptr req, response::ping::handler cb, io_id_t 
 
 void pingpong::ping2(request::ping::ptr req, response::ping::handler cb, io_id_t io_id, pong_handler pong_reqester) 
 {
+  req->ping_count++;
   if ( _targets.empty() )
   {
     ::wamba::pingpong::pong_send_( std::move(req), std::move(cb), pong_reqester );
@@ -172,44 +180,119 @@ void pingpong::ping2(request::ping::ptr req, response::ping::handler cb, io_id_t
 
 void pingpong::pong(request::pong::ptr req, response::pong::handler cb ) 
 {
-  PINGPONG_LOG_MESSAGE("void pingpong::pong(request::pong::ptr req, response::pong::handler cb )")
-
+  req->pong_count++;
   if ( cb == nullptr ) 
     return;
 
   auto res = std::make_unique<response::pong>();
   res->ping_count = req->ping_count;
-  res->pong_count = req->pong_count+1;
+  res->pong_count = req->pong_count;
   cb( std::move(res) );
 }
 
 void pingpong::stress_ping_( )
 {
   //PINGPONG_LOG_MESSAGE("void pingpong::stress_ping_( size_t stress_ping)")
+  _stat_time = std::chrono::high_resolution_clock::now();
   using namespace std::placeholders;
   size_t size = _stress_ping;
   for ( size_t i=0; i < size; ++i)
   {
     auto req = std::make_unique<request::ping>();
+    req->ping_count = -1; // Локальный не считаем
     auto start = std::chrono::high_resolution_clock::now();
     this->ping2(std::move(req), this->wrap( std::bind(&pingpong::stress_result_, this, _1, start) ) , 0, nullptr );
   }
 }
 
-void pingpong::stress_result_( response::ping::ptr, std::chrono::high_resolution_clock::time_point start )
+void pingpong::stress_result_( response::ping::ptr res, std::chrono::high_resolution_clock::time_point start )
 {
+  if ( res == nullptr )
+  {
+    PINGPONG_LOG_MESSAGE("stress_result_: BAD GATEWAY")
+    return;
+  }
+  
   auto finish = std::chrono::high_resolution_clock::now();
-  //auto interval = start - finish;
   auto tm_ms = std::chrono::duration_cast< std::chrono::nanoseconds >( finish - start ).count();
-  auto rate = tm_ms > 0 ? 1000000000.0/tm_ms : -1 ;
-  PINGPONG_LOG_MESSAGE("STRESS RESULT!!! " << rate << " " << tm_ms)
+
+  this->stat_(tm_ms, res->ping_count, res->pong_count);
+  
   this->global()->io_service.post( this->wrap( [this]()
   {
     auto start = std::chrono::high_resolution_clock::now();
     using namespace std::placeholders;
     auto req = std::make_unique<request::ping>();
+    req->ping_count = -1; // Локальный не считаем
     this->ping2(std::move(req), this->wrap( std::bind(&pingpong::stress_result_, this, _1, start) ) , 0, nullptr );
   }));
+}
+
+static inline long ns2rate(time_t ns, int count = 1)
+{
+  return ns > 0 ? 1000000000.0*count/ns : -1;
+}
+
+static inline std::string rate_fmt(std::vector<time_t>& i, size_t perc, size_t k)
+{
+  size_t pos = i.size()*perc / 100;
+  if ( pos == i.size() )
+    pos -= 1; 
+    
+  auto rate = ns2rate(i[pos]);
+  std::stringstream ss;
+  ss << perc << "% "
+  << std::setprecision(3) <<  std::fixed
+  << i[pos]/1000000.0 << "ms " << std::internal << rate << "("<< rate*k <<")ps\t";
+  return ss.str();
+}
+
+void pingpong::stat_( time_t ms, size_t pingc, size_t pongc )
+{
+  _stat_count += 1;
+  std::vector<time_t> i;
+  time_t now = time(0);
+  {
+    std::lock_guard<mutex_type> lk( _mutex );
+    
+    if ( _show_time == now )
+      return;
+      
+    _intervals.push_back(ms);
+    if ( _intervals.size() == 1000 )
+    {
+      _intervals.swap(i);
+      _intervals.reserve(1000);
+    }
+  }
+
+  
+  
+  if ( !i.empty() )
+  {
+    auto stat_finish = std::chrono::high_resolution_clock::now();
+    auto tm_ns = std::chrono::duration_cast< std::chrono::nanoseconds >( stat_finish - _stat_time ).count();
+    
+    size_t k = (pingc + pongc)*2; // Количество сообщений запросы+ответы
+    std::sort(i.begin(), i.end());
+
+    PINGPONG_LOG_MESSAGE(
+      rate_fmt(i, 0, k) <<
+      rate_fmt(i, 50, k) <<
+      rate_fmt(i, 80, k) <<
+      rate_fmt(i, 95, k) <<
+      rate_fmt(i, 99, k) <<
+      rate_fmt(i, 100, k) )
+    PINGPONG_LOG_MESSAGE( 
+      "Totals "<< pingc << "<->" << pongc
+      << std::setprecision(3) << std::fixed
+      << " pings " << _stat_count << " time " <<  tm_ns/1000000.0
+      << " rate " << ns2rate(tm_ns, _stat_count) << " rate" << k << " " << ns2rate(tm_ns, _stat_count)*k 
+    )
+    _show_time = now;
+    _stat_time = std::chrono::high_resolution_clock::now();
+    _stat_count = 0;
+  }
 }
 
 }}
