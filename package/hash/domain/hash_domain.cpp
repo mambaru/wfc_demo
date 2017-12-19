f11724a30798f67665ec6c417027f5a69466f394
Vladimir Migashko <migashko@gmail.com>
2017-12-19 19:59:46 +0300

    *wlog

--- package/hash/domain/hash_domain.cpp
+++ package/hash/domain/hash_domain.cpp
@@ -5,12 +5,17 @@
 //
 
 #include "hash_domain.hpp"
+#include <hash/logger.hpp>
 #include <unistd.h>
+#include <wfc/logger.hpp>
+#include <wfc/wfc_exit.hpp>
 
 namespace demo{ namespace hash{
 
 void hash_domain::initialize()
 {
+  //HASH_LOG_FATAL("Демо хеш кранты!")
+  //wfc_exit_with_error("Демо хеш кранты!");
   /*
   std::thread([this](){
     double x = 0.1;
@@ -33,6 +38,13 @@ void hash_domain::get_hash(request::get_hash::ptr req, response::get_hash::handl
 
 void hash_domain::perform_io(data_ptr d, io_id_t, output_handler_t handler)
 {
+  HASH_LOG_MESSAGE("Демо hash_domain::perform_io!")
+  COMMON_LOG_MESSAGE("Демо hash_domain::perform_io!")
+  HASH_LOG_WARNING("Демо hash_domain::perform_io!")
+  COMMON_LOG_WARNING("Демо hash_domain::perform_io!")
+  
+  //HASH_LOG_FATAL("Демо хеш кранты!")
+  
   std::string str( d->begin(), d->end() );
   size_t val = std::hash< std::string >()( str );
   std::string valstr = std::to_string(val);
