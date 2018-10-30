147e06809759c6672dbf06a94d32918df55c6232
Vladimir Migashko <migashko@gmail.com>
2018-10-30 19:13:23 +0300

    *hash

--- package/hash/domain/hash_domain.cpp
+++ package/hash/domain/hash_domain.cpp
@@ -11,6 +11,8 @@
 #include <wfc/logger/ilogger.hpp>
 #include <wfc/wfc_exit.hpp>
 #include <wlog/logger_fun.hpp>
+#include <iow/logger.hpp>
+
 
 namespace demo{ namespace hash{
 
@@ -37,13 +39,13 @@ public:
 
 void hash_domain::configure() 
 {
-  this->set_target("logger", "IOW", std::make_shared<logger>() );
+  ///! this->set_target("logger", "IOW", std::make_shared<logger>() );
   DEBUG_LOG_DEBUG("hash_domain::configure " << this->options().param)
 }
 
 void hash_domain::reconfigure() 
 {
-  this->set_target("logger", "IOW", std::make_shared<logger>() );
+  ///! this->set_target("logger", "IOW", std::make_shared<logger>() );
   DEBUG_LOG_DEBUG("hash_domain::reconfigure " << this->options().param)
 }
 
@@ -68,24 +70,6 @@ void hash_domain::get_hash(request::get_hash::ptr req, response::get_hash::handl
 
 void hash_domain::perform_io(data_ptr d, io_id_t, output_handler_t handler)
 {
-  /*
-  HASH_LOG_WARNING(this->has_arg("abort"))
-  HASH_LOG_WARNING(this->get_arg("abort"))
-  if ( this->get_arg_t<bool>("abort") )
-  {
-    abort();
-  }
-  else
-  {
-    HASH_LOG_WARNING("this->get_arg_t<bool>('abort')")
-  }
-
-  HASH_LOG_MESSAGE("Демо hash_domain::perform_io!")
-  COMMON_LOG_MESSAGE("Демо hash_domain::perform_io!")
-  HASH_LOG_WARNING("Демо hash_domain::perform_io!")
-  COMMON_LOG_WARNING("Демо hash_domain::perform_io!")
-  */
-  
   if (d==nullptr)
     return handler(nullptr);
   
@@ -94,13 +78,6 @@ void hash_domain::perform_io(data_ptr d, io_id_t, output_handler_t handler)
   std::string valstr = std::to_string(val);
   auto res = std::make_unique<data_type>( valstr.begin(), valstr.end() );
   handler( std::move(res) );
-  /*
-  if ( str=="fatal" ) { COMMON_LOG_FATAL("hash_domain::perform_io") }
-  if ( str=="exit" ) 
-  {
-    COMMON_LOG_WARNING("hash_domain::perform_io") 
-    wfc_exit();
-  }*/
 }
 
 }}
